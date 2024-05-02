94c9acaee594b403ed125299d4cae65bd486d2e3
ALVAROPING1 <alvaroping1@gmail.com>
2024-05-02 18:10:53 +0200

    problem 251 solution

---
+++ problems/251 Parkímetros.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+// Read positive number
+template <typename T> void fastscan(T& number) {
+    number = 0;
+    // Keep on extracting characters if they are integers
+    for (register int c = getchar_unlocked(); (c >= '0' && c <= '9');
+         c = getchar_unlocked())
+        number = number * 10 + c - '0';
+}
+
+// Max value of a single coin
+const size_t max_value = 200;
+// Max amount of types of coins
+const size_t max_types = 10;
+
+int main() {
+    // Fast IO
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    // Init arrays
+    std::array<int, max_value * max_types + 1> values_table;
+    std::array<int, max_types> types;
+    int n;
+    fastscan(n);
+    while (n--) {
+        int n_types, max_coins;
+        fastscan(n_types);
+        fastscan(max_coins);
+        int max_value = 0;
+        // Read the types of steps
+        for (int i = 0; i < n_types; ++i) {
+            fastscan(types[i]);
+            max_value = std::max(max_value, types[i]);
+        }
+        values_table.fill(100);
+        // Base case
+        values_table[0] = 0;
+        // Recursive cases
+        int end = max_value * max_coins;
+        for (int i = 0; i < end; ++i) {
+            int curr = values_table[i] + 1;
+            if (curr > max_coins) continue;
+            for (int j = 0; j < n_types; ++j) {
+                int value = types[j];
+                values_table[i + value] =
+                    std::min(values_table[i + value], curr);
+            }
+        }
+        int total = 0;
+        for (int i = 1; i <= end; ++i)
+            total += values_table[i] <= max_coins;
+        // Print the result and reset the steps table
+        std::cout << total << "\n";
+    }
+    return 0;
+}
