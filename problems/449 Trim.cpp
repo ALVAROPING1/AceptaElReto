03e1dffbc39e447a51ead9d733a6375e41ac7735
ALVAROPING1 <alvaroping1@gmail.com>
2024-06-09 20:20:51 +0200

    improved problem 449 solution again
    Thanks Cesar

--- problems/449 Trim.cpp
+++ problems/449 Trim.cpp
@@ -4,12 +4,13 @@
 const int MAX = 81;
 
 template <typename T> inline int fastscan(T& line) {
-    register int i = 0;
+    register int i = -1;
     register int c = getchar_unlocked();
     if (c == EOF) return 0;
     for (; c != '\n'; c = getchar_unlocked())
-        line[i++] = c;
-    line[i] = '\0';
+        // Preprocess word by deleting duplicate characters
+        if (line[i] != c) line[++i] = c;
+    line[++i] = '\0';
     return i;
 }
 
@@ -44,10 +45,6 @@ template <typename T> inline void fastprint(T number) {
 //     3F     1
 
 int main() {
-    // Fast IO
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-
     char input[MAX];
     short calls[MAX - 1][MAX - 1];
     // Base cases. We can preset them as they are never modified
@@ -60,33 +57,13 @@ int main() {
         // We don't need to clear the dynamic table because only positions that
         // have already been calculated/written for this line are read
         // Recursive cases
-        for (int end = 1; end < len; ++end) {
-            for (int start = end - 1; start >= 0; --start) {
-                // Start and end characters of the current substring are equal
-                if (input[end] == input[start] and end - start != 1) {
-                    // Find the start/end of the substring of the trimmed word
-                    int end2 = end - 1, start2 = start + 1;
-                    while (input[end2] == input[end])
-                        --end2;
-                    while (input[start2] == input[start])
-                        ++start2;
-                    // If the result is an empty word, a single trim is needed.
-                    // Otherwise, we need a trim plus the cost of the trimmed
-                    // word
-                    calls[end][start] =
-                        end2 >= start2 ? calls[end2][start2] + 1 : 1;
-                } else {
-                    // Get the minimum cost of removing the first or last
-                    // character. If the character is the same as the next
-                    // inner character, there is no extra cost (it was already
-                    // paid for by the innermost character of that sequence)
-                    calls[end][start] = std::min(
-                        calls[end][start + 1] +
-                            (input[start] != input[start + 1]),
-                        calls[end - 1][start] + (input[end] != input[end - 1]));
-                }
-            }
-        }
+        for (int end = 1; end < len; ++end)
+            for (int start = end - 1; start >= 0; --start)
+                calls[end][start] = (input[end] == input[start]
+                                         ? calls[end - 1][start + 1]
+                                         : std::min(calls[end][start + 1],
+                                                    calls[end - 1][start])) +
+                                    1;
         // Print the result and read the next line
         fastprint(calls[len - 1][0]);
         len = fastscan(input);
