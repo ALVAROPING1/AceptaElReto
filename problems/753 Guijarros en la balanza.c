176001d9e1a546eb485389a3a947ef9ba7d70b77
ALVAROPING1 <alvaroping1@gmail.com>
2024-06-25 00:55:00 +0200

    problem 753 solution

---
+++ problems/753 Guijarros en la balanza.c
@@ -0,0 +1,75 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+void fastscan(int* number) {
+    *number = 0;
+    register int c = getchar_unlocked();
+    // Keep on extracting characters if they are integers
+    for (; (c >= '0' && c <= '9'); c = getchar_unlocked())
+        *number = *number * 10 + c - '0';
+}
+
+// Maximum amount of weights
+#define MAX 12
+
+// For some reason these variables being global results in a much faster time
+int weights[MAX];
+int n;
+int target;
+
+struct pair {
+    int pos, current;
+};
+
+int main() {
+    struct pair stack[1 << MAX];
+    fastscan(&n);
+    while (n != 0) {
+        int total = 0;
+        int i = n;
+        // Read all the weights and calculate target value of each group
+        while (i--) {
+            fastscan(&weights[i]);
+            total += weights[i];
+        }
+        target = total / 2;
+        // Initialize brute force search
+        stack[0].pos = 0;
+        stack[0].current = 0;
+        i = 0;
+        // DFS through all possible combinations of weights taken
+        while (1) {
+            // If the current weight is the target, it's possible and we can
+            // stop the search
+            if (stack[i].current == target) {
+                putchar_unlocked('S');
+                putchar_unlocked('I');
+                goto end;
+            };
+            // If we have already surpased the target value or made a choice for
+            // all weights, this path doesn't lead to a solution. Pop the
+            // current state and continue with the next
+            if (stack[i].current > target || stack[i].pos == n) {
+                --i;
+                if (i < 0) break;
+                continue;
+            };
+            // Pop the current state since it has already been processed
+            int pos = stack[i].pos;
+            int curr = stack[i].current;
+            // Push the successor states to the stack: the next weight can
+            // either be taken or not. Push the take choice second so it's
+            // processed earlier as it's more likely to reach an end earlier
+            stack[i].pos = pos + 1;
+            stack[i].current = curr;
+            stack[++i].pos = pos + 1;
+            stack[i].current = curr + weights[pos];
+        }
+        putchar_unlocked('N');
+        putchar_unlocked('O');
+    end:
+        putchar_unlocked('\n');
+        fastscan(&n);
+    }
+    return 0;
+}
