8fb4b26288e2c489319001b8ce42cdfbcb6ebe3b
ALVAROPING1 <alvaroping1@gmail.com>
2025-06-27 13:48:56 +0200

    improved problem 811 solution

--- problems/811 Positivo, negativo y cero.cpp
+++ problems/811 Positivo, negativo y cero.cpp
@@ -8,6 +8,16 @@ inline void fastscan_positive(int* number) {
         *number = *number * 10 + c - '0';
 }
 
+#define MAX 300000
+
+// ╔═══════════════════════════════════════════════════════════════════════════╗
+// ║                           Segment Tree solution                           ║
+// ╚═══════════════════════════════════════════════════════════════════════════╝
+
+// Use a segment tree to store the values to be able to perform range queries
+// and value updates in O(n log n)
+
+// Quickly scan if the next number is positive, negative, or zero
 inline void fastscan_sign(int* number) {
     register int c = getchar_unlocked();
     if (c == '-') *number = -1;
@@ -22,11 +32,6 @@ inline int mul(int l, int r) {
     return l * r;
 }
 
-#define MAX 300000
-
-// TODO: try if slight implementation variations improve speed
-// - 1-indexing
-// - Memory optimization
 class SegmentTree {
   protected:
     using size_type = ptrdiff_t;
@@ -109,7 +114,7 @@ class SegmentTree {
     }
 };
 
-int main() {
+int solve1() {
     int n, c;
     fastscan_positive(&n);
     fastscan_positive(&c);
@@ -147,4 +152,134 @@ int main() {
         fastscan_positive(&c);
     }
     return 0;
+}
+
+// ╔═══════════════════════════════════════════════════════════════════════════╗
+// ║                           Fenwick Tree solution                           ║
+// ╚═══════════════════════════════════════════════════════════════════════════╝
+
+// Use a simpler fenwick tree to store the values and be able to perform
+// updates/queries. Has the same complexity as a segment tree, but it's faster
+// in practice due to being simpler (which also makes it more restrictive)
+//
+// Fenwick trees can only answer queries for ranges [0, r], for ranges [l, r]
+// the ranges for [0, l-1] and [0, r] must be combined. We can store the amount
+// of zeros and negative numbers in each range and perform a subtraction for the
+// combination. The result of the multiplication of the numbers in the range
+// will be:
+// - 0 if there are any zeros
+// - -1 if the amount of negative numbers is odd
+// - 1 otherwise
+
+// Struct to store the counts
+struct Value {
+    int zeros, negatives;
+};
+
+// Quickly scan a number and convert it to the `Value` struct
+inline void fastscan_sign(Value* number) {
+    register int c = getchar_unlocked();
+    if (c == '-') *number = {0, 1};
+    else if (c == '0') *number = {1, 0};
+    else *number = {0, 0};
+    while (getchar_unlocked() >= '0') {}
+}
+
+class FenwickTree {
+  protected:
+    Value bit_[MAX]; // binary indexed tree
+    int n_;
+
+  public:
+    FenwickTree(int n) {
+        n_ = n;
+        for (int i = 0; i < n; ++i)
+            bit_[i] = Value{0, 0};
+    }
+
+    FenwickTree(int size, Value a[]) : FenwickTree(size) {
+        for (int i = 0; i < n_; i++) {
+            bit_[i].zeros += a[i].zeros;
+            bit_[i].negatives += a[i].negatives;
+            int r = i | (i + 1);
+            if (r < n_) {
+                bit_[r].zeros += bit_[i].zeros;
+                bit_[r].negatives += bit_[i].negatives;
+            }
+        }
+    }
+
+    Value query(int l, int r) {
+        Value res = query(r);
+        Value res2 = query(l - 1);
+        res.zeros -= res2.zeros;
+        res.negatives -= res2.negatives;
+        return res;
+    }
+
+    Value query(int r) {
+        Value ret = {0, 0};
+        for (; r >= 0; r = (r & (r + 1)) - 1) {
+            ret.zeros += bit_[r].zeros;
+            ret.negatives += bit_[r].negatives;
+        }
+        return ret;
+    }
+
+    void update(int idx, Value delta) {
+        for (; idx < n_; idx = idx | (idx + 1)) {
+            bit_[idx].zeros += delta.zeros;
+            bit_[idx].negatives += delta.negatives;
+        }
+    }
+};
+
+int solve2() {
+    int n, c;
+    fastscan_positive(&n);
+    fastscan_positive(&c);
+
+    Value values[MAX];
+    while (n != 0 && c != 0) {
+        // Read the starting values and initialize the fenwick tree
+        // Since we only ever care about the sign of values, there is no
+        // need to actually parse the number. Getting its sign and ignoring the
+        // input until the next number/new line is enough
+        for (int i = 0; i < n; ++i)
+            fastscan_sign(&values[i]);
+        FenwickTree tree{n, values};
+        // For each operation
+        while (c--) {
+            char op = getchar_unlocked(); // Get its type
+            getchar_unlocked();           // Ignore space
+            if (op == 'C') {              // Change value
+                int pos;
+                Value val;
+                fastscan_positive(&pos);
+                fastscan_sign(&val);
+                --pos;
+                // Calculate the difference in the counts
+                Value delta = {val.zeros - values[pos].zeros,
+                               val.negatives - values[pos].negatives};
+                values[pos] = val; // Update the value in the original array
+                tree.update(pos, delta); // Apply the update in the fenwick tree
+            } else {                     // Query range
+                int start, end;
+                fastscan_positive(&start);
+                fastscan_positive(&end);
+                Value res = tree.query(start - 1, end - 1);
+                if (res.zeros > 0) putchar_unlocked('0');
+                else if (res.negatives % 2 == 1) putchar_unlocked('-');
+                else putchar_unlocked('+');
+            }
+        }
+        putchar_unlocked('\n');
+        fastscan_positive(&n);
+        fastscan_positive(&c);
+    }
+    return 0;
+}
+
+int main() {
+    return solve2();
 }
