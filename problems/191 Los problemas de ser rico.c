970e4c0ea78b99005cd0f9661d396738b8d465a9
ALVAROPING1 <alvaroping1@gmail.com>
2025-12-29 14:51:49 +0100

    problem 191 solution

---
+++ problems/191 Los problemas de ser rico.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+inline void fastscan(int* number) {
+    register int c;
+    *number = 0;
+    c = getchar_unlocked();
+    for (; (c >= '0' && c <= '9'); c = getchar_unlocked())
+        *number = *number * 10 + c - '0';
+}
+
+inline void fastprint(int number) {
+    char buffer[16];
+    register int i = -1;
+    do {
+        buffer[++i] = (number % 10) + '0';
+        number /= 10;
+    } while (number);
+    while (i >= 0)
+        putchar_unlocked(buffer[i--]);
+    putchar_unlocked('\n');
+}
+
+int main() {
+    int n;
+    fastscan(&n);
+    while (n--) {
+        int compartments, capacity, diff;
+        fastscan(&compartments);
+        fastscan(&capacity);
+        fastscan(&diff);
+        int total = 0;
+        while (compartments--) {
+            total += capacity;
+            capacity -= diff;
+        }
+        fastprint(total);
+    }
+}
